85480cbb6091164f0d158974e0e5dfc53c86edbe
Philippe SIM <psim@e3r3p10.42.fr>
2019-11-13 17:59:50 +0100

    updated snapshot

--- client/components/Layout/index.tsx
+++ client/components/Layout/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import TopMenu from '../TopMenu'
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+import TopMenu from '../TopMenu';
 
 import styles from './styles.css';
 
@@ -13,7 +13,7 @@ import styles from './styles.css';
 const Layout = ({ children }) => (
   <div>
 
-    <TopMenu></TopMenu>
+    <TopMenu />
 
   </div>
 );
