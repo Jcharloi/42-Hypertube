44af7cfd543ec8264b90006a8868a7a9a9652ec3
Florian Muller <fmuller@student.42.fr>
2019-11-22 18:08:57 +0100

    spliting SignUp component
    with a new SignUpForm component

--- client/components/Authentication/SignUp.tsx
+++ client/components/Authentication/SignUp.tsx
@@ -1,20 +1,19 @@
 import React, { ReactElement, ChangeEvent, useState } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
 
 import { useIntl } from 'react-intl';
+import useStyles from './SignUp.styles';
+
+import SigneUpForm from './SignUpForm';
 
 import {
   UserInfo, UserError, requiredErrorKey, requiredPictureErrorKey, checkRequiredField,
   validateEmail, validatePassword, validatePicture, isThereError, sendSignUpData,
 } from './SignUp.service';
 
-import useStyles from './SignUp.styles';
 
 const errosToRemoveOnChange = [
   requiredErrorKey,
@@ -87,6 +86,7 @@ const SignUp = (): ReactElement => {
    */
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
+    // todo: keep error like mail/usrname taken ?
     const newUserError = checkRequiredField(userInfo);
     newUserError.email = userInfo.email === '' || validateEmail(userInfo.email) ? newUserError.email : 'authentication.signUp.error.email.invalid';
     newUserError.password = userInfo.password === '' || validatePassword(userInfo.password) ? newUserError.password : 'authentication.signUp.error.password.invalid';
@@ -126,125 +126,14 @@ const SignUp = (): ReactElement => {
           </Grid>
         </Grid>
 
+        <SigneUpForm
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+          userInfo={userInfo}
+          userError={userError}
+          waitingRes={waitingRes}
+        />
 
-        <form onSubmit={handleSubmit}>
-          <Grid container direction="column" alignItems="center">
-
-            {/* Text input */}
-            <Grid item className={classes.item}>
-              <TextField
-                value={userInfo.username}
-                helperText={userError.username !== '' ? _t({ id: userError.username }) : ''}
-                error={userError.username !== ''}
-                name="username"
-                onChange={handleInputChange}
-                label={_t({ id: 'authentication.signUp.username' })}
-                variant="filled"
-                className={classes.textInput}
-                inputProps={{ maxLength: 30 }}
-                autoFocus
-              />
-            </Grid>
-            <Grid item className={classes.item}>
-              <TextField
-                value={userInfo.password}
-                helperText={userError.password !== '' ? _t({ id: userError.password }) : ''}
-                error={userError.password !== ''}
-                name="password"
-                onChange={handleInputChange}
-                label={_t({ id: 'authentication.signUp.password' })}
-                variant="filled"
-                className={classes.textInput}
-                inputProps={{ maxLength: 1028 }}
-                type="password"
-              />
-            </Grid>
-            <Grid item className={classes.item}>
-              <TextField
-                value={userInfo.email}
-                helperText={userError.email !== '' ? _t({ id: userError.email }) : ''}
-                error={userError.email !== ''}
-                name="email"
-                onChange={handleInputChange}
-                label={_t({ id: 'authentication.signUp.email' })}
-                variant="filled"
-                className={classes.textInput}
-                inputProps={{ maxLength: 100 }}
-              />
-            </Grid>
-            <Grid item className={classes.item}>
-              <TextField
-                value={userInfo.firstName}
-                helperText={userError.firstName !== '' ? _t({ id: userError.firstName }) : ''}
-                error={userError.firstName !== ''}
-                name="firstName"
-                onChange={handleInputChange}
-                label={_t({ id: 'authentication.signUp.firstName' })}
-                variant="filled"
-                className={classes.textInput}
-                inputProps={{ maxLength: 30 }}
-              />
-            </Grid>
-            <Grid item className={classes.item}>
-              <TextField
-                value={userInfo.lastName}
-                helperText={userError.lastName !== '' ? _t({ id: userError.lastName }) : ''}
-                error={userError.lastName !== ''}
-                name="lastName"
-                onChange={handleInputChange}
-                label={_t({ id: 'authentication.signUp.lastName' })}
-                variant="filled"
-                className={classes.textInput}
-                inputProps={{ maxLength: 30 }}
-              />
-            </Grid>
-
-            {/* Picture upload */}
-            <Grid container direction="column" alignItems="center" className={classes.item}>
-              <Grid item>
-                <Typography variant="body1" gutterBottom>
-                  {_t({ id: 'authentication.signUp.picture' })}
-                  <span role="img" aria-label="Arm taking a selfie"> 🤳</span>
-                </Typography>
-              </Grid>
-              <Grid item>
-                <label htmlFor="raised-button-file">
-                  <input
-                    accept="image/*"
-                    style={{ display: 'none' }}
-                    id="raised-button-file"
-                    multiple
-                    type="file"
-                    name="picture"
-                    onChange={handleInputChange}
-                  />
-                  <Button variant="outlined" color="secondary" startIcon={<CloudUploadIcon />} component="span">
-                    {_t({ id: 'authentication.signUp.uploadButton' })}
-                  </Button>
-                </label>
-              </Grid>
-              {userError.picture !== ''
-                && (
-                  <Grid item>
-                    <Typography variant="body2" className={classes.pictureErrorMsg}>
-                      {_t({ id: userError.picture })}
-                    </Typography>
-                  </Grid>
-                )}
-            </Grid>
-
-            {/* Send form */}
-            <Grid item className={classes.item}>
-              <div className={classes.circularProgressContainer}>
-                <Button type="submit" disabled={waitingRes} variant="contained" size="large" color="primary">
-                  {!waitingRes && _t({ id: 'authentication.signUp.sendButton' })}
-                  {waitingRes && _t({ id: 'authentication.signUp.sendButton.waiting' })}
-                </Button>
-                {waitingRes && (<CircularProgress color="secondary" className={classes.circularProgress} />)}
-              </div>
-            </Grid>
-          </Grid>
-        </form>
       </Grid>
     </Paper>
   );
