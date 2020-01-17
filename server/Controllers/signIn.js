5ef55eae95ef0b4cdcf4ae8c7419c2fdcaafbbae
Florian Muller <fmuller@student.42.fr>
2020-01-18 00:30:13 +0100

    back login verification
    checking username/password

--- server/Controllers/signIn.js
+++ server/Controllers/signIn.js
@@ -1,6 +1,25 @@
+import bcrypt from "bcrypt";
+import UserModel from "../Schemas/User";
+
 const signIn = async (req, res) => {
-  console.log(req.body);
-  res.status(200).send();
+  const { username, password } = req.body;
+
+  if (username && password) {
+    try {
+      const user = await UserModel.findOne({ username }, "password");
+      if (user && (await bcrypt.compare(password, user.password))) {
+        // todo: set cookie with jwt here
+        res.status(200).send();
+      } else {
+        res.status(400).send({ error: "BAD_CREDENTIALS" });
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).send();
+    }
+  } else {
+    res.status(400).send();
+  }
 };
 
 export default signIn;
