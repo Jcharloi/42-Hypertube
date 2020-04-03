ad8f43f9c2e5a379ca49174076c60b6fa6f7f6a9
hcasanov <noelledeur@gmail.com>
2020-04-03 17:21:37 +0200

    New HomePage
    Not finish : We must to check if the url work well after route search modification.

--- client/components/Home/index.tsx
+++ client/components/Home/index.tsx
@@ -1,32 +1,85 @@
-import React, { ReactElement } from "react";
-import Typography from "@material-ui/core/Typography";
-
+/* eslint-disable prettier/prettier */
+import React, { ReactElement, useState } from "react";
+import { Link } from "react-router-dom";
+import Axios from "axios";
 import { useHomeStyles } from "./styles";
-
-import Feed from "./Feed";
-import RecentVideos from "./RecentVideos";
-import Film from "./FeaturedMovie";
 
 const Home = (): ReactElement => {
   const classes = useHomeStyles({});
+  const [item, setItem] = useState(null);
+  const [refresh, setRefresh] = useState(true);
+  const [itemReady, setItemReady] = useState(false);
+
+  const getRecommendation = (): void => {
+    const list: Array<object> = [];
+    for (let index = 0; index < 4; index++) {
+      Axios(`https://tv-v2.api-fetch.website/movies/1`).then((res) => {
+        const id = Math.round(Math.random() * 50);
+        console.log(id);
+        list.push({
+          title: res.data[id].title,
+          img: res.data[id].images.poster,
+          imdbId: res.data[id].imdb_id,
+          genres: res.data[id].genres,
+          src: `http://localhost:8080/movie/${res.data[id].imdbId}`
+        });
+        setItem(list);
+        if (index === 3) {
+          setItemReady(true);
+        }
+      });
+    }
+  };
+
+  if (refresh) {
+    setRefresh(false);
+    getRecommendation();
+  }
 
   return (
-    <div className={classes.container}>
-      <div className={classes.header}>
-        <Typography variant="h1">Welcome Julien</Typography>
-        <Typography variant="h2">Start browsing videos</Typography>
-      </div>
-      <div className={classes.mainContent}>
-        <div className={classes.feedContainer}>
-          <Feed />
+    <div className={classes.mainPoster}>
+      {itemReady ? (
+        <div className={classes.boxContent}>
+          <Link className={classes.miniaturePoster} to={item[0].src}>
+            <img
+              className={classes.miniatureImage}
+              src={item[0].img}
+              alt="Movie poster"
+            />
+            <h2 className={classes.movieTitle}>{item[0].title}</h2>
+            <h3 className={classes.genre}>({item[0].genres[0]})</h3>
+          </Link>
+          <Link className={classes.miniaturePoster} to={item[1].src}>
+            <img
+              className={classes.miniatureImage}
+              src={item[1].img}
+              alt="Movie poster"
+            />
+            <h2 className={classes.movieTitle}>{item[1].title}</h2>
+            <h3 className={classes.genre}>({item[1].genres[0]})</h3>
+          </Link>
+          <Link className={classes.miniaturePoster} to={item[2].src}>
+            <img
+              className={classes.miniatureImage}
+              src={item[2].img}
+              alt="Movie poster"
+            />
+            <h2 className={classes.movieTitle}>{item[2].title}</h2>
+            <h3 className={classes.genre}>({item[2].genres[0]})</h3>
+          </Link>
+          <Link className={classes.miniaturePoster} to={item[3].src}>
+            <img
+              className={classes.miniatureImage}
+              src={item[3].img}
+              alt="Movie poster"
+            />
+            <h2 className={classes.movieTitle}>{item[3].title}</h2>
+            <h3 className={classes.genre}>({item[3].genres[0]})</h3>
+          </Link>
         </div>
-        <div className={classes.filmContainer}>
-          <Film />
-        </div>
-      </div>
-      <div>
-        <RecentVideos />
-      </div>
+      ) : (
+          ""
+        )}
     </div>
   );
 };
--- client/components/Home/styles.ts
+++ client/components/Home/styles.ts
@@ -1,116 +1,48 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export const useHomeStyles = makeStyles((theme) => ({
-  container: {
-    padding: theme.spacing(4)
+export const useHomeStyles = makeStyles(() => ({
+  mainPoster: {
+    background:
+      "linear-gradient(to bottom, transparent 40%, black 80%), url('https://fr.web.img6.acsta.net/pictures/19/11/19/12/39/2279585.jpg')",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "50% 0%",
+    width: "100%",
+    height: "100%",
+    marginTop: "0",
+    display: "flex"
   },
-  header: {
-    marginBottom: theme.spacing(3)
-  },
-  mainContent: {
-    paddingBottom: theme.spacing(2),
+  boxContent: {
+    marginTop: "23%",
+    width: "100%",
     display: "flex",
-    height: "60%"
+    flexWrap: "wrap"
   },
-  feedContainer: {
-    flex: 2,
+  miniaturePoster: {
+    height: "400px",
+    width: "280px",
+    bottom: "0",
+    marginBottom: "120px",
+    marginRight: "auto",
+    marginLeft: "auto",
+    display: "flex",
+    cursor: "pointer",
+    flexWrap: "wrap",
+    textDecoration: "none",
+    color: "white"
+  },
+  miniatureImage: {
+    width: "100%",
     height: "100%"
   },
-  filmContainer: {
-    flex: 1,
-    height: "100%",
-    marginLeft: theme.spacing(2)
-  }
-}));
-
-export const useFeedStyles = makeStyles((theme) => ({
-  feedContainer: {
-    height: "100%",
-    padding: theme.spacing(2)
-  },
-  recentFeedContainer: {
-    display: "flex",
-    padding: theme.spacing(1)
-  },
-  infos: {
-    marginLeft: theme.spacing(1)
-  },
-  header: {
-    marginBottom: theme.spacing(1),
-    marginLeft: theme.spacing(1)
-  }
-}));
-
-export const useRecentMoviesStyle = makeStyles((theme) => ({
-  container: {
-    padding: theme.spacing(3)
-  },
-  recentFilmContainer: {
-    display: "flex",
-    justifyContent: "space-evenly",
-    padding: theme.spacing(3)
-  },
-  recentFilm: {
-    position: "relative",
-    transition: "transform 0.2s",
-    "&:hover": {
-      transform: "scale(1.15)"
-    }
-  },
-  recentFilmTitle: {
-    color: theme.palette.text.primary,
-    userSelect: "none",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    top: 0,
-    left: 0,
-    height: "100%",
+  movieTitle: {
     width: "100%",
-    opacity: 0,
-    zIndex: 10,
-    transition: "background 0.2s, opacity 0.2s",
-    "&:hover": {
-      opacity: 1,
-      background: "rgba(0,0,0,0.75)"
-    }
+    margin: "0",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans- serif"
+  },
+  genre: {
+    margin: "0"
   }
 }));
 
-export const useFilmStyles = makeStyles((theme) => ({
-  container: {
-    height: "100%",
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    textAlign: "center",
-    padding: theme.spacing(2)
-  },
-  thumbnail: {
-    marginTop: theme.spacing(3),
-    alignSelf: "center"
-  },
-  ratingContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "self-end",
-    margin: theme.spacing(1, 0)
-  },
-  descriptionContainer: {
-    textAlign: "justify",
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-    height: "20vh",
-    overflowY: "auto"
-  },
-  tag: {
-    margin: theme.spacing(0.5)
-  },
-  buttonContainer: {
-    marginTop: theme.spacing(4),
-    alignSelf: "center",
-    width: "30%"
-  }
-}));
+export const useRecentMoviesStyle = makeStyles(() => ({}));
